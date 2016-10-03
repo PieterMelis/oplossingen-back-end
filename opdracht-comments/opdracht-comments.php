315a060c146d19a9f869a8ddf171d7931d21e712
pieter melis <pieter.melis@hotmail.com>
2016-10-03 16:10:44 +0200

    First commit Message

---
+++ opdracht-comments/opdracht-comments.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <?php /*
+      Pieter Melis
+      melis.pieter@student.kdg.be
+
+    */ ?>
+    <title></title>
+  </head>
+  <body>
+    <section>
+      <?php $naam = "Pieter Melis" ?>
+
+      <p><?php echo $naam; ?></p>
+
+  </section>
+  </body>
+</html>
