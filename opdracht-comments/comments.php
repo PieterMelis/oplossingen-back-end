78fe4f4d6e73355d48608799941aaf6c6111b663
pieter melis <pieter.melis@hotmail.com>
2016-10-03 15:42:45 +0200

    Revert "Revert "Eerste opdracht""
    This reverts commit f07794e815fce06f2bf140c3df7e5456038a196b.

---
+++ opdracht-comments/comments.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    /*
+      Pieter Melis
+      melis.pieter@student.kdg.be
+
+    */
+    <title></title>
+  </head>
+  <body>
+    <section>
+      <p><?php var_dump( $naam ) ?></p>
+      <p><?php echo "Melis" ?></p>
+
+  </section>
+  </body>
+</html>
