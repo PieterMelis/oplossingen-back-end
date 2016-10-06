398113fbe221db1a3382b5772937769030015550
pieter melis <pieter.melis@hotmail.com>
2016-10-06 17:07:31 +0200

    opdracht-arrays-basis

---
+++ opdracht-arrays-basis/opdracht-arrays-basis.php
@@ -0,0 +1,17 @@
+<?php
+$dierenLijst1 		= 	array( 'koe', 'uil', 'kip', 'leeuw', 'gans', 'aap', 'walvis', "haai", "slang", "tijger" );
+$dierenLijst2[0] = 'koe';
+$dierenLijst2[1] = 'uil';
+$dierenLijst2[2] = 'kip';
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
