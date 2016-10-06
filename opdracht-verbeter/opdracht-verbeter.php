6603e87c5548f4b0182ab525af9f68cc7f133967
pieter melis <pieter.melis@hotmail.com>
2016-10-06 16:53:31 +0200

    https://github.com/PieterMelis/oplossingen-back-end.git

---
+++ opdracht-verbeter/opdracht-verbeter.php
@@ -0,0 +1,19 @@
+<?php
+	$tekst 			= 	'Test 123...';
+	$tekst2 		= 	'Test 456';
+	$tekst3 		= 	'test';
+	$zin 			= 	'Jean D\'Haeseleir gaat naar Praag!!';
+	$beschrijving 	= 	"Vogelbekdier";
+?>
+
+<!DOCTYPE html>
+<mtml>
+<head></head>
+<body>
+
+	<h1>Al de fouten zijn aangepast</h1>
+
+
+
+</body>
+</html>
