99470671dd579d0a3013f22e86a7524380920b5d
pieter melis <pieter.melis@hotmail.com>
2016-10-06 17:29:02 +0200

    opdracht-array bezig

--- opdracht-arrays-basis/opdracht-arrays-basis.php
+++ opdracht-arrays-basis/opdracht-arrays-basis.php
@@ -1,9 +1,19 @@
 <?php
-$dierenLijst1 		= 	array( 'koe', 'uil', 'kip', 'leeuw', 'gans', 'aap', 'walvis', "haai", "slang", "tijger" );
+$dierenLijst1 	 = 	array( 'koe', 'uil', 'kip', 'leeuw', 'gans', 'aap', 'walvis', "haai", "slang", "tijger" );
 $dierenLijst2[0] = 'koe';
 $dierenLijst2[1] = 'uil';
 $dierenLijst2[2] = 'kip';
+$dierenLijst2[3] = 'leeuw';
+$dierenLijst2[4] = 'gans';
+$dierenLijst2[5] = 'aap';
+$dierenLijst2[6] = 'walvis';
+$dierenLijst2[7] = 'haai';
+$dierenLijst2[8] = 'slang';
+$dierenLijst2[9] = 'tijger';
 
+$voertuigenLijst = array(	'landvoertuigen' 	=> 	array(),
+                      'watervoertuigen' => 	array(),
+                      'luchtvoertuigen' => 	array() );
  ?>
 <!DOCTYPE html>
 <html>
