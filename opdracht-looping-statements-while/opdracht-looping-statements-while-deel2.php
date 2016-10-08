b8c45d837bd946d7c8e7dd4efb71e2176b2031b1
pieter melis <pieter.melis@hotmail.com>
2016-10-08 19:12:02 +0200

    opdracht-looping-statements-while

---
+++ opdracht-looping-statements-while/opdracht-looping-statements-while-deel2.php
@@ -0,0 +1,86 @@
+<?php
+$boodschappenlijstje                = array('melk',
+                                            'koekjes',
+                                            'Martini',
+                                            'Aardappelen',
+                                            'Bakpapier',
+                                            'Vruchtensap',
+                                            'Shampoo',
+                                            '6 eieren',
+                                            'Kookroom',
+                                            'WC papier',
+                                            'Keukenrol',
+                                            'Gehakt',
+                                            'Kipfilet',
+                                            'Tomaten',
+                                            'Komkommer',
+                                            'Winterpeen',
+                                            'Avocado',
+                                            'Condooms',
+                                            'Sla',
+                                            'Kaas',
+                                            'Mozzarella',
+                                            'Rogge brood',
+                                            'Ontbijt koek',
+                                            'Bruin stokbrood',
+                                            'Appels',
+                                            'Peren',
+                                            'Sinasappels',
+                                            'Bananen',
+                                            'Mandarijnen / kiwi’s',
+                                            'Perziken/ pruimen',
+                                            'Komkommers',
+                                            'Tomaten',
+                                            'Paprika’s',
+                                            'Wortels',
+                                            'Uien',
+                                            'Bloemkool',
+                                            'Broccoli',
+                                            'Prei',
+                                            'Courgettes',
+                                            'Aubergine',
+                                            'Knoflook',
+                                            'Champignons',
+                                            'Oesterzwammen',
+                                            'Paddenstoelen',
+                                            'Honing',
+                                            'Kaneelpoeder',
+                                            'Cacao poeder',
+                                            'Rozijnen',
+                                            'Walnoten',
+                                            'Amandelnoten',
+                                            'Pecannoten',
+                                            'Zwarte chocolade',
+                                            'Tutti-Frutti ( = gedroogd fruit)',
+                                            'Zalm',
+                                            'Markeel',
+                                            'Garnalen',
+                                            'Pangasius',
+                                            'Haring',
+                                            'Anjovis',
+                                            'Tonijn',
+                                            'Sardines',
+                                            'Kipfilet',
+                                            'Kipgehakt',
+                                            'Kalkoen',
+
+                                          );
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>While deel 2</h1>
+    <h3>boodschappen </h3>
+    <ul>
+      <?php foreach ($boodschappenlijstje as $value): ?>
+         <li> <?= $value ?></li>
+      <?php endforeach ?>
+    </ul>
+
+  </body>
+</html>
