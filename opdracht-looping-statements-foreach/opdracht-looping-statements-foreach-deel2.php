a777a9ae78e255a20d6e7f4240fdfa3f92ad63a7
pieter melis <pieter.melis@hotmail.com>
2016-10-09 21:50:53 +0200

    opdracht-looping-statements-foreach

---
+++ opdracht-looping-statements-foreach/opdracht-looping-statements-foreach-deel2.php
@@ -0,0 +1,19 @@
+<?php
+$tekst = file_get_contents("text-file.txt");
+$a = 0;
+$tekst = strtoupper($tekst);
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>foreach-deel-2</title>
+  </head>
+  <body>
+    <h1>foreach deel 2</h1>
+    <?php foreach (count_chars($tekst, 1) as $key => $value): ?>
+      <li><?= chr($key) ?> X <?= $value ?>   </li>
+    <?php endforeach; ?>
+  </body>
+</html>
