db6c80a1161060ebaf00b0b9973547f388df289d
pieter melis <pieter.melis@hotmail.com>
2016-10-20 16:39:59 +0200

    date klaar

--- opdracht-date/opdracht-date-deel2.php
+++ opdracht-date/opdracht-date-deel2.php
@@ -1,19 +1,18 @@
-
 <?php
-
+	setlocale( 'LC_ALL', 'nld_nld' );
 $timestamp =       mktime(22,35,25,01,21,1904);
 
- $date =          date('l jS \of F Y h:i:s A', $timestamp );
+ $date =          strftime('%d %B %Y, %H:%M:%S %p', $timestamp );
  ?>
 
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Date deel 1</title>
+    <title>Date deel 2</title>
   </head>
   <body>
-    <h1>Date deel 1</h1>
+    <h1>Date deel 2</h1>
     <h3><?=$date?></h3>
   </body>
 </html>
