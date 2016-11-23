5e7ffcdb9583701019f8bce13994b213406ecebf
pieter melis <pieter.melis@hotmail.com>
2016-11-23 12:15:30 +0100

    include-require done

--- opdracht-include-require/index.php
+++ opdracht-include-require/index.php
@@ -2,22 +2,22 @@
   $artikels = array(
     array(
       "title" => "PHP 8.4 gereleased",
-      "text" => "De nieuwste versie van PHP bevat uitbreidingen op oa. mind control-facades, in-browser 3d projecties en de API voor drones.",
-      "tags" => array(
+      "text"  => "De nieuwste versie van PHP bevat uitbreidingen op oa. mind control-facades, in-browser 3d projecties en de API voor drones.",
+      "tags"  => array(
         "new", "PHP 8.4"
       )
     ),
     array(
       "title" => "Zymphovel framework",
-      "text" => "Wordt Zymphovel het nieuwste PHP-framework dat de wereld in een sneltempo zal veroveren?",
-      "tags" => array(
+      "text"  => "Wordt Zymphovel het nieuwste PHP-framework dat de wereld in een sneltempo zal veroveren?",
+      "tags"  => array(
         "frameworks"
       )
     ),
     array(
       "title" => "Rasmus Lerdorf vermist",
-      "text" => "De geestelijke vader van PHP Rasmus Lerdorf is sinds vorige week vermist nadat hij met zijn privé-jet richting Ibiza vertrok.",
-      "tags" => array(
+      "text"  => "De geestelijke vader van PHP Rasmus Lerdorf is sinds vorige week vermist nadat hij met zijn privé-jet richting Ibiza vertrok.",
+      "tags"  => array(
         "bizar", "rasmus", "lerdorf", "feestneus"
       )
     )
