29fb7873b36e61a6003772ede9a1311d65a6dc4e
pieter melis <pieter.melis@hotmail.com>
2016-12-04 10:17:22 +0100

    bezig

--- opdracht-CRUD-delete/opdracht-CRUD-delete.php
+++ opdracht-CRUD-delete/opdracht-CRUD-delete.php
@@ -1,14 +1,76 @@
 <?php
-//Maak een connectie met de database en selecteer de database bieren.
-$db = new PDO('mysql:host=localhost;dbname=bieren', 'root', '' );
+
+$message	=	"";
+
+try
+{
+  $db     = new PDO('mysql:host=localhost;dbname=bieren', 'root', '' );
+  $bieren	=	array();
+  $uno    = 1;
+
+  $queryString = 'SELECT * FROM brouwers';;
+  $brouwers = $db->prepare($queryString);
+  $brouwers->execute();
+  $brouwArr = array();
+  while ($brouwernaam = $brouwers->fetch(PDO::FETCH_ASSOC))
+  {
+      $brouwArr[] = $brouwernaam;
+  }
+
+}
+
+    catch (Exception $e)
+    {
+        $message = "Connectie is niet gelukt.";
+    }
+
+
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Opdracht CRUD query</title>
   </head>
-  <body>
+  <style media="screen">
+    td{
+      border: 1px  solid black;
+      margin: 0;
+      padding: 1em;
+      background: lightgrey;
+    }
 
-  </body>
+  </style>
+
+  <body>
+    <?php echo $message?>
+    <h1>Opdracht CRUD delete</h1>
+
+    <table>
+
+        <thead>
+          <tr>
+                 <th>biernummer</th>
+                 <th>bier</th>
+                 <th>adres</th>
+                 <th>postcode</th>
+                 <th>gemeente</th>
+                 <th>omzet</th>
+
+             </tr>
+        </thead>
+
+        <tbody>
+          <?php foreach ($brouwArr as $key => $bier): ?>
+             <tr>
+                 <?php foreach ($bier as $naam): ?>
+                 <td><?= $naam ?></td>
+                 <?php endforeach ?>
+                 <td><input type="image" src="img/icon-delete.png" border="0" alt="Submit" value="<?= $key ?>" /></td>
+             </tr>
+         <?php endforeach ?>
+        </tbody>
+
+    </table>
+</body>
 </html>
