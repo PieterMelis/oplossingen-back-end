123b2fd7f75ba801c4f8e68b0d83efc340f52200
pieter melis <pieter.melis@hotmail.com>
2016-12-15 23:57:46 +0100

    img +

---
+++ opdract-security-login/gegevens-wijzigen-form.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>gegevens-wijzigen-form</title>
+    <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+  </body>
+</html>
