9689a4d22b5e29b3585188fbe3bcbc0f9c7e98ac
pieter melis <pieter.melis@hotmail.com>
2016-12-16 12:28:09 +0100

    html aangepast en oplalen van info cookie

--- opdracht-file-upload/gegevens-wijzigen-form.php
+++ opdracht-file-upload/gegevens-wijzigen-form.php
@@ -2,44 +2,29 @@
 session_start();
 $user = array();
 $login=false;
-if(isset($_COOKIE['login'])){
 
   $user = explode(",",$_COOKIE["login"]);
-  $database = database($user[0]);
 
 
-if($database == $user[1])
-      {
-        $login=true;
-      }
-      else
-      {
-        $_SESSION["errorLogin"]= "werkt niet ";
-        header('location: login-form.php' );
-      }
-
-    }
-
-      function database($email)
-      {
-        // Connectie tot database
         try {
           $db = new PDO("mysql:host=localhost;dbname=opdracht-security-login", "root", "");
 
           $queryCheckUser = "SELECT * FROM users where email = :email";
-          $checkUser = $db->prepare($queryCheckUser);
-          $checkUser->bindValue(":email", $email);
-          $checkUser->execute();
-          $result = $checkUser->fetch(PDO::FETCH_ASSOC);
+          $statement = $db->prepare($queryCheckUser);
+          $statement->bindValue(":email", $user[0]);
+          $statement->execute();
+          $fetchRowLog = array();
+          while ( $row = $statement->fetch(PDO::FETCH_ASSOC) )
+          {
+            $fetchRowLog[]	=	$row;
+          }
+          var_dump( $fetchRowLog);
 
-          $_SESSION["test"] = $result["hashed_password"];
-          return $result["hashed_password"];
         } catch (Exception $e) {
-          $_SESSION["errorLogin"]="Er is iets fout met de database";
+          $_SESSION["errorLogin"]="Er is iets fout met de database en kan img niet vinden";
           header('location: login-form.php' );
-          $login=false;
+
         }
-      }
 
 
 ?>
@@ -52,14 +37,27 @@ if($database == $user[1])
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <?php if ($login): ?>
       <p><a  href="dashboard.php">Terug naar dashboard</a> | Ingelogd als <?= $user[0]; ?> | <a  href="logout.php">uitloggen</a></p>
       <h1 >Gegevens wijzigen</h1>
       <br>
       <p>Profielfoto</p>
+      <form>
+        <ul>
+            <li>
+                <label for="profile_picture">
+                    <img class="profile-picture" src="" alt="Profielfoto">
+                </label>
+                <input type="file" id="profile_picture" name="profile_picture">
+            </li>
 
+            <li>
+                <label for="email">e-mail</label>
+                <input type="text" id="email" name="email" value="<?= $user[0]; ?>">
+            </li>
+        </ul>
+        <input type="submit" value="Gegevens wijzigen">
+    </form>
 
-    <?php endif; ?>
 
   </body>
 </html>
